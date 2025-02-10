f52f28cd1ff6168201309b1535796099bd2d3c3a
GotsulyakEgor <egorgotsulyak2@gmail.com>
2025-02-10 23:53:40 +0200

    Update db.config.js

--- node-mongodb-server/app/config/db.config.js
+++ node-mongodb-server/app/config/db.config.js
@@ -1,3 +1,7 @@
+// module.exports = {
+//   url: "mongodb://localhost:27017/BME-catalog"
+// };
 module.exports = {
-  url: "mongodb://localhost:27017/BME-catalog"
+  url: "mongodb+srv://egorgotsulyak:Wbm22g2xWem56avy@cluster0.emsdi.mongodb.net/BME-catalog?retryWrites=true&w=majority&appName=Cluster0"
 };
+
